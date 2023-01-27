cede1cfdf0b8a2ea0c1ed0e467d7a640607ee1b3
Guy Marshall <guymarshall9@gmail.com>
2023-01-27 20:35:10 +0000

    Add code for program

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-/*
-    Navigate to parent directory of this project, then for each directory, run "cargo clean".
-    Print output in terminal
-*/
+use std::process::Command;
+use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    let current_dir = std::env::current_dir().unwrap();
+    let parent_dir = current_dir.parent().unwrap();
+    let program_dir = std::env::current_exe().unwrap();
+    let directories = fs::read_dir(parent_dir).unwrap();
+
+    for dir in directories {
+        let dir = dir.unwrap();
+        let path = dir.path();
+
+        if path.is_dir() && path != program_dir.parent().unwrap() {
+            let output = Command::new("cargo")
+                .arg("clean")
+                .current_dir(&path)
+                .output()
+                .expect("Failed to run cargo clean");
+
+            println!("{:?}", output);
+        }
+    }
 }
