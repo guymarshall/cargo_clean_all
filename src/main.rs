5f0c81891cdf98945b6115ce5dff31f4d6479e6a
Guy Marshall <guymarshall9@gmail.com>
2023-03-09 22:13:50 +0000

    Add type annotations + imports

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use std::process::Command;
+use std::process::{Command, Output};
 use std::fs;
+use std::fs::{DirEntry, ReadDir};
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let current_dir = std::env::current_dir().unwrap();
-    let parent_dir = current_dir.parent().unwrap();
-    let program_dir = std::env::current_exe().unwrap();
-    let directories = fs::read_dir(parent_dir).unwrap();
+    let current_dir: PathBuf = std::env::current_dir().unwrap();
+    let parent_dir: &Path = current_dir.parent().unwrap();
+    let program_dir: PathBuf = std::env::current_exe().unwrap();
+    let directories: ReadDir = fs::read_dir(parent_dir).unwrap();
 
     for dir in directories {
-        let dir = dir.unwrap();
-        let path = dir.path();
+        let dir: DirEntry = dir.unwrap();
+        let path: PathBuf = dir.path();
 
         if path.is_dir() && path != program_dir.parent().unwrap() {
-            let output = Command::new("cargo")
+            let output: Output = Command::new("cargo")
                 .arg("clean")
                 .current_dir(&path)
                 .output()
